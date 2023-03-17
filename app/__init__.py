4b87ab8e3b813fa952c930a3e1d1ecc2ee98ed73
herrera-luis <herrera_luis10@hotmail.com>
2023-03-16 22:57:23 -0500

    feat: removing not used code

--- app/__init__.py
+++ app/__init__.py
@@ -24,26 +24,20 @@ class JSONLogFormatter(logging.Formatter):
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    # Logging configuration
+
     log_level = app.config.get('LOG_LEVEL', logging.INFO)
     log_dir = app.config.get('LOG_DIR', 'logs')
     log_file = os.path.join(log_dir, 'todo_service.log')
     os.makedirs(log_dir, exist_ok=True)
-
     file_handler = RotatingFileHandler(
         log_file, maxBytes=10240, backupCount=10)
     file_handler.setFormatter(JSONLogFormatter())
     file_handler.setLevel(log_level)
     app.logger.addHandler(file_handler)
     app.logger.setLevel(log_level)
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO
+                        )
     db.init_app(app)
-
-    # with app.app_context():
-    #     migrate = Migrate(app, db)
-    #     # Apply migrations at startup
-    #     upgrade()
-
     register_routes(app)
 
     return app
